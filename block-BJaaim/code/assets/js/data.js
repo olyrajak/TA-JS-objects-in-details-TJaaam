2f73ca0f9bf31d89abc78879f041a48b14614cc1
Oly Rajak <olyrajak@gmail.com>
2022-09-11 17:52:52 +0530

    Complete Assignment: Quiz App Using OOP

---
+++ block-BJaaim/code/assets/js/data.js
@@ -0,0 +1,63 @@
+const questionnire = [{
+        title: "Who was the first female president of India ?",
+        options: [
+            "Indira Gandhi",
+            "Sucheta Kripalani",
+            "Pratibha Devi Singh Patil ",
+            "Vijya Lakshmi Pandit",
+        ],
+        answerIndex: 2,
+    },
+
+    {
+        title: "On what date was the Emancipation Proclamation issued in 1863?",
+        options: ["January 1st", "July 4th", "December 24th", "December 25th"],
+        answerIndex: 2,
+    },
+
+    {
+        title: "Which battle resulted in the most casualties for US soldiers?",
+        options: ["D-Day", "Battle of Antietam", "Pearl Harbor Attack", "X-Day"],
+        answerIndex: 0,
+    },
+    {
+        title: "Which is the Smallest State In India ?",
+        options: ["Goa", "West Bengal", "Andhra Pradesh", "Chhattisgarh"],
+        answerIndex: 0,
+    },
+    {
+        title: "Which is the smallest river in India ?",
+        options: ["Arvari river", "Krishna", "Krishna", "Rhine"],
+        answerIndex: 0,
+    },
+    {
+        title: "Name the longest river in the world?",
+        options: ["Amazon", "Ganga", "Nile", "Rhine"],
+        answerIndex: 2,
+    },
+    {
+        title: "What city do The Beatles come from?",
+        options: ["London", "Liverpool", "Madrid", "Barcelona"],
+        answerIndex: 1,
+    },
+    {
+        title: "Who invented the World Wide Web, and when?",
+        options: [
+            "Larry Page, 1990",
+            "Larry Page, 1992",
+            "Tim Berners-Lee, 1992",
+            "Tim Berners-Lee, 1990",
+        ],
+        answerIndex: 3,
+    },
+    {
+        title: "Until 1923, what was the Turkish city of Istanbul called?",
+        options: ["Basutoland", "Constantinopole", "Formosa", "Turicum"],
+        answerIndex: 1,
+    },
+    {
+        title: "Which Indian city is known as The Venice of The East?",
+        options: ["Kunnur", "Alappuzha", "Kochi", "Kozhikode"],
+        answerIndex: 1,
+    },
+];
